d0726afadf700996684463ead8346feaf19bc450
Sujeevan Rajagopalan <rsujee@MA307.local>
2020-12-08 15:31:23 +0000

    step:14 post ed params

--- sinatra/app.rb
+++ sinatra/app.rb
@@ -15,8 +15,12 @@ get '/random-cat' do
   erb(:index)
 end
 
-get '/named-cat' do
+post '/named-cat' do
   p params
   @name = params[:name]
   erb(:index)
+end
+
+get '/cat-name' do
+  erb(:form)
 end
