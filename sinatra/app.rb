7174d969ad393a1610dd42ffd1881310fbfc00e8
Chris Abdo <christophermabdo@icloud.com>
2020-12-08 15:06:54 +0000

    step13: using forms complete

--- sinatra/app.rb
+++ sinatra/app.rb
@@ -10,7 +10,13 @@ get '/secret' do
   "Cats control the world"
 end
 
-get '/cat' do
-  @random_name = ['Amigo', 'Oscar', "Viking"].sample
+get '/random-cat' do
+  @name = ['Amigo', 'Oscar', "Viking"].sample
+  erb(:index)
+end
+
+get '/named-cat' do
+  p params
+  @name = params[:name]
   erb(:index)
 end
